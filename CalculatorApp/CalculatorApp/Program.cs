37a9344732d4a0154b6142f4f349519b55e3137b
Latha Paidi <paidi.latha@tcs.com>
2016-06-16 12:14:29 +0000

    commit sample solution to Github master branch

---
+++ CalculatorApp/CalculatorApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorLib;
+
+namespace CalculatorApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Calculator App");
+      Calculator cl = new Calculator();
+      int c2 = cl.Add(2,3);
+      Console.WriteLine("Result:" + c2);
+    }
+  }
+}
