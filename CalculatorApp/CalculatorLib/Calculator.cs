0e697bfb601d2cc576bbeca9c61f66307a7e4471
Latha Paidi <paidi.latha@tcs.com>
2016-06-17 10:35:03 +0000

    added comments to calculator class

--- CalculatorApp/CalculatorLib/Calculator.cs
+++ CalculatorApp/CalculatorLib/Calculator.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace CalculatorLib
 {
+  /// <summary>
+  /// Calculator class
+  /// </summary>
   public class Calculator
   {
+    /// <summary>
+    /// Add method
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns>integer</returns>
     public int Add(int a ,int b)
     {
       return a + b;
